a2b6828a29c7f6b7608311324237a39703d9e5ff
alexthedar <alex.castaneda@gmail.com>
2017-02-19 14:04:32 -0800

    complete CRUD but non persistent

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -4,17 +4,18 @@
 
   angular
     .module("classifieds")
-    .controller("classifiedsCTRL", function ($scope, $http, classifiedsFactory, $mdSidenav) {
+    .controller("classifiedsCTRL", function ($scope, $http, classifiedsFactory, $mdSidenav, $mdToast, $mdDialog) {
 
+      // READ
       classifiedsFactory.getClassifieds()
         .then(function(classifieds) {
           $scope.classifieds = classifieds.data;
         })
 
       var contact = {
-        name: "shit brain",
+        name: "test test",
         phone: "555-555-5555",
-        email: "kill@myself.com"
+        email: "test@test.com"
       }
       $scope.openSidebar = function(){
         $mdSidenav('left').open()
@@ -23,12 +24,53 @@
         $mdSidenav('left').close()
       }
 
+      //CREATE
       $scope.saveClassified = function(classified) {
         if(classified ){
+          classified.contact = contact
           $scope.classifieds.push(classified);
           $scope.classified = {};
-          $scope.closeSidebar()
+          $scope.closeSidebar();
+          showToast("Classified Saved")
         }
+      }
+      
+      // UPDATE
+      $scope.editClassified = function(classified){
+        $scope.editing = true
+        $scope.openSidebar()
+        $scope.classified = classified
+      }
+
+      $scope.saveEdit = function(){
+        $scope.editing = false
+        $scope.classified = {}
+        $scope.closeSidebar()
+        showToast("Edit Saved")
+      }
+
+      // DELETE
+      $scope.deleteClassified = function(event, classified){
+        var confirm = $mdDialog.confirm()
+                      .title("Are you sure you want to delete "+ classified.title+" ?")
+                      .ok('Yes')
+                      .cancel('No')
+                      .targetEvent(event)
+
+        $mdDialog.show(confirm).then(function(){
+          var index = $scope.classifieds.indexOf(classified)
+          $scope.classifieds.splice(index, 1)
+        }, function(){  })
+
+      }
+
+      function showToast(message){
+        $mdToast.show(
+          $mdToast.simple()
+            .content(message)
+            .position("top, right")
+            .hideDelay(3000)
+        )
       }
     });
 })();
