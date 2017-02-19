419e066329cefbc6b64e8033d47948495e92af71
alexthedar <alex.castaneda@gmail.com>
2017-02-18 22:44:46 -0800

    classified ctr

---
+++ components/classifieds.ctr.js
@@ -0,0 +1,15 @@
+(function() {
+
+  "use strict";
+
+  angular
+    .module("classifieds")
+    .controller("classifiedsCTRL", function ($scope) {
+
+      $scope.name = {
+        first: "skdjkas",
+        last: "sads"
+      };
+
+    });
+})();
