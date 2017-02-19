34fe9c8a6b8f92ecd4dab38d4c70a14a7f3c369b
alexthedar <alex.castaneda@gmail.com>
2017-02-19 13:22:31 -0800

    adding crud

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -4,12 +4,31 @@
 
   angular
     .module("classifieds")
-    .controller("classifiedsCTRL", function ($scope, $http, classifiedsFactory) {
+    .controller("classifiedsCTRL", function ($scope, $http, classifiedsFactory, $mdSidenav) {
 
       classifiedsFactory.getClassifieds()
         .then(function(classifieds) {
           $scope.classifieds = classifieds.data;
         })
 
+      var contact = {
+        name: "shit brain",
+        phone: "555-555-5555",
+        email: "kill@myself.com"
+      }
+      $scope.openSidebar = function(){
+        $mdSidenav('left').open()
+      }
+      $scope.closeSidebar = function(){
+        $mdSidenav('left').close()
+      }
+
+      $scope.saveClassified = function(classified) {
+        if(classified ){
+          $scope.classifieds.push(classified);
+          $scope.classified = {};
+          $scope.closeSidebar()
+        }
+      }
     });
 })();
