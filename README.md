2a60934e17e0c99d8b02f27eeed9242d690f792d
alexthedar <alex.castaneda@gmail.com>
2017-02-20 12:35:03 -0800

    redo git

---
+++ README.md
@@ -0,0 +1,22 @@
+# Angular 1.x Material Classifieds
+
+###### Practice Site by Alexthedar
+
+[Click Here For Live Site](link)
+
+> A classifieds site with Angular-Material design and full CRUD capability using Firebase.
+
+
+Install and Run:
+
+
+```bash
+git clone [repo]
+
+cd [directory]
+
+npm install
+
+npm run start
+
+```
---
+++ scripts/app.js
@@ -0,0 +1,37 @@
+angular
+  .module('classifieds', ["ngMaterial", "ui.router", "firebase"])
+  .config(function($mdThemingProvider, $stateProvider) {
+
+    $mdThemingProvider.theme('default')
+      .primaryPalette('teal')
+      .accentPalette('orange')
+
+      // var classifiedsState = {
+      //   name: 'classifieds',
+      //   url: '/classifieds',
+      //   templateUrl: './components/classifieds/classifieds.tpl.html',
+      //   controller: 'classifiedsCTRL as vm'
+      // }
+  $stateProvider.state('classifieds', {
+                          url: '/classifieds',
+                          templateUrl: './components/classifieds/classifieds.tpl.html',
+                          controller: 'classifiedsCTRL as vm'})
+                .state('classifieds.new', {
+                          url: '/new',
+                          templateUrl: './components/classifieds/new/classifieds.new.tpl.html',
+                          controller: 'newClassifiedsCTRL as vm'})
+                .state('classifieds.edit', {
+                          url: '/edit/:id',
+                          templateUrl: './components/classifieds/edit/classifieds.edit.tpl.html',
+                          controller: 'editClassifiedsCTRL as vm',
+                          params: {
+                            classified: null
+                          }
+                        })
+                .state('default', {
+                          url: '',
+                          templateUrl: './components/classifieds/classifieds.tpl.html',
+                          controller: 'classifiedsCTRL as vm'
+                        })
+
+  })
